f61d3d1d712eeff8b6141adbb4dd236f1e753237
Mattias Ryrlén <mattias@ryrlen.org>
2019-08-22 16:24:47 +0200

    Gem: Add default value to unused for a gem with no stats.
    Signed-off-by: Mattias Ryrlén <mattias@ryrlen.org>

--- Gem.py
+++ Gem.py
@@ -1,8 +1,8 @@
 class Gem:
     def __init__(self):
-        self.bonus_type = ''
+        self.bonus_type = 'Unused'
         self.bonus_attribute = ''
-        self.bonus_amount = ''
+        self.bonus_amount = '0'
 
     def __repr__(self):
         return "Type: '{}', Attribute: '{}', Amount: '{}'".format(self.bonus_type, self.bonus_attribute, self.bonus_amount)
